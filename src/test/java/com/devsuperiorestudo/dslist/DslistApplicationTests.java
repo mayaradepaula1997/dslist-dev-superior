5aafeeb1653d8588ed7b53a2318d36530cf73216
Mayara <mayaradepaula1997>
2025-08-19 23:20:08 -0300

    first commit

---
+++ src/test/java/com/devsuperiorestudo/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorestudo.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
