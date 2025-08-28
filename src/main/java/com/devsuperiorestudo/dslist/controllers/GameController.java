54bf63ae8fe340c1bd7b64d740fcb9cdda69205d
Mayara <mayaradepaula1997>
2025-08-27 22:45:12 -0300

    GET game by id

--- src/main/java/com/devsuperiorestudo/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperiorestudo/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devsuperiorestudo.dslist.controllers;
 
+import com.devsuperiorestudo.dslist.dtos.GameDTO;
 import com.devsuperiorestudo.dslist.dtos.GameMinDTO;
 import com.devsuperiorestudo.dslist.entities.Game;
 import com.devsuperiorestudo.dslist.service.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class GameController {
 
     public GameController(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
     }
 
     @GetMapping
--- src/main/java/com/devsuperiorestudo/dslist/service/GameService.java
+++ src/main/java/com/devsuperiorestudo/dslist/service/GameService.java
@@ -1,9 +1,11 @@
 package com.devsuperiorestudo.dslist.service;
 
+import com.devsuperiorestudo.dslist.dtos.GameDTO;
 import com.devsuperiorestudo.dslist.dtos.GameMinDTO;
 import com.devsuperiorestudo.dslist.entities.Game;
 import com.devsuperiorestudo.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,17 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
+    public GameDTO findById(Long id){
+
+        Game result =  gameRepository.findById(id).get(); //busco o game no banco de dados e retorno ele
+        GameDTO dto = new GameDTO(result); //instancio o GameDTO, passando o Game no seu construtor
+
+        return dto;
+
+    }
+
+
+
     public List<GameMinDTO> findAll(){
 
        List<Game> result = gameRepository.findAll();
@@ -24,4 +37,6 @@ public class GameService {
        return dto;
 
     }
+
+
 }
